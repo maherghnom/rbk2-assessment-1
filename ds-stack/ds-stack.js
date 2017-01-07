19484872789324967f4c22fcff15d3231a8ec5ea
Admin <m2ghanom@gmail.com>
2017-01-07 09:33:09 +0200

    stack solving

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,17 @@
 var Stack = function() {
 	//your code is here
+	Instance=Object.create(Stackmethods);
+	Instance._storage=[];
+
+
+	return Instance;
 };
+Stackmethods={}
+
+Stackmethods.add=function(value){
+	this._storage.push(value)
+
+}
+Stackmethods.remove=function(){
+	this._storage.pop()
+}
